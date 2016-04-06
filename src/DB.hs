9313bc08bbc13e993a5eafe18005ee0eec5566a6
joelkarli <joel.karli91@gmail.com>
2016-04-06 21:10:41 +0200

    init db

--- src/DB.hs
+++ src/DB.hs
@@ -1,7 +1,29 @@
+import System.Environment
 import Database.HDBC
 import Database.HDBC.Sqlite3
 
 main :: IO()
 main = do
-    conn <- connectSqlite3 "chestega.db"
+    args <- getArgs
+    conn <- connectSqlite3 $ last args
+    initTable conn
+    commit conn
     disconnect conn
+
+initTable conn = do
+                tables <- getTables conn
+                if "games" `elem` tables then run conn deleteGamesQuery []
+                                         else run conn createTableQuery []
+
+createTableQuery = "CREATE TABLE games (\
+                   \white VARCHAR(255) NOT NULL,\
+                   \black VARCHAR(255) NOT NULL,\
+                   \day INTEGER,\
+                   \month INTEGER,\
+                   \year INTEGER,\
+                   \event VARCHAR(255),\
+                   \site VARCHAR(255),\
+                   \result VARCHAR(255),\
+                   \PRIMARY KEY (white, black));"
+
+deleteGamesQuery = "DELETE FROM games;"

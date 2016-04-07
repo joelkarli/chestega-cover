f87410464d156e03cb990f4fd33a3f45222943f8
joelkarli <joel.karli91@gmail.com>
2016-04-07 22:39:59 +0200

    refactoring pgn parser

--- src/PGN.hs
+++ src/PGN.hs
@@ -2,62 +2,35 @@ module PGN
 ( 
 ) where
 
+import Data.Char
 import qualified Text.Parsec as P
 import Control.Monad.Identity (Identity)
 import Game
 
 parse rule = P.parse rule "(source)"
 
-gameParser :: P.ParsecT String () Identity [String]
+gameParser :: P.ParsecT String () Identity [(String, String)]
 gameParser = do
-            event <- eventParser
-            site <- siteParser
-            date <- dateParser
-            white <- whiteParser
-            black <- blackParser
-            result <- resultParser
+            mapping <- P.many attrLineParser
             P.char '\n'
             annotation <- annotationParser
-            return [event, site, date, white, black, result, annotation]
+            return (("annotation", annotation) : mapping)
 
+pgnAttrParser :: P.ParsecT String () Identity (String, String)
+pgnAttrParser = do
+            P.char '['
+            key <- P.many P.letter
+            P.spaces
+            P.char '"'
+            value <- P.manyTill (P.noneOf "\"\n") (P.char '"')
+            P.char ']'
+            return (key, value)
 
-
-pgnAttrParser :: String -> P.ParsecT String () Identity String
-pgnAttrParser s = do
-                P.char '['
-                P.string s
-                P.spaces
-                P.char '"'
-                value <- P.manyTill (P.noneOf "\"\n") (P.char '"')
-                P.string "]\n"
-                return value
-
-unimportantAttrParser :: P.ParsecT String () Identity String
-unimportantAttrParser = do
-                    P.char '['
-                    P.many P.letter
-                    P.spaces
-                    P.char '"'
-                    P.many (P.noneOf "\"\n")
-                    P.string "\"]\n"
+attrLineParser :: P.ParsecT String () Identity (String, String)
+attrLineParser = do
+            pair <- pgnAttrParser
+            P.char '\n'
+            return pair
 
 annotationParser :: P.ParsecT String () Identity String
-annotationParser = P.manyTill (P.noneOf "") (P.string "\n\n")
-
-whiteParser :: P.ParsecT String () Identity String
-whiteParser = pgnAttrParser "White"
-
-blackParser :: P.ParsecT String () Identity String
-blackParser = pgnAttrParser "Black"
-
-dateParser :: P.ParsecT String () Identity String
-dateParser = pgnAttrParser "Date"
-
-eventParser :: P.ParsecT String () Identity String
-eventParser = pgnAttrParser "Event"
-
-siteParser :: P.ParsecT String () Identity String
-siteParser = pgnAttrParser "Site"
-
-resultParser :: P.ParsecT String () Identity String
-resultParser = pgnAttrParser "Result"
+annotationParser = P.manyTill (P.anyChar) (P.string "\n\n")

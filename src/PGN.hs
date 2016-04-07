38a61b2b0cd4b40a1ac2adc1d15d349aef61ecd9
joelkarli <joel.karli91@gmail.com>
2016-04-07 08:46:20 +0200

    comment parser

--- src/PGN.hs
+++ src/PGN.hs
@@ -6,7 +6,7 @@ import qualified Text.Parsec as P
 import Control.Monad.Identity (Identity)
 import Game
 
-parse rule text = P.parse rule "source.txt" text
+parse rule = P.parse rule "(source)"
 
 pgnAttrParser :: String -> P.ParsecT String () Identity String
 pgnAttrParser s = do
@@ -16,7 +16,13 @@ pgnAttrParser s = do
                 P.char '"'
                 event <- P.manyTill (P.noneOf "\"\n") (P.char '"')
                 P.char ']'
+                P.char '\n'
                 return event
+
+commentParser :: P.ParsecT String () Identity String
+commentParser = do
+            P.char '{'
+            P.manyTill (P.noneOf "}") (P.char '}')
 
 whiteParser :: P.ParsecT String () Identity String
 whiteParser = pgnAttrParser "White"

2c454d5df2ee2e14100d544e7bb2a9d8e21aaae9
joelkarli <joel.karli91@gmail.com>
2016-04-08 01:37:22 +0200

    write pgn file to db

--- src/DB.hs
+++ src/DB.hs
@@ -2,12 +2,14 @@ import System.Environment
 import Database.HDBC
 import Database.HDBC.Sqlite3
 import PGN
+import Game
 
 main :: IO()
 main = do
     args <- getArgs
     conn <- connectSqlite3 $ last args
     initTable conn
+    updateTable args conn
     commit conn
     disconnect conn
 
@@ -15,6 +17,14 @@ initTable conn = do
                 tables <- getTables conn
                 if "games" `elem` tables then run conn deleteGamesQuery []
                                          else run conn createTableQuery []
+
+updateTable args conn = do
+                pgn <- readFile filename
+                stmt <- prepare conn insertGameQuery
+                executeMany stmt (gamesToSqlList (pgnToGames filename pgn))
+                where filename = head $ tail args
+
+gamesToSqlList games = [[toSql (white g), toSql (black g), toSql (date g), toSql (event g), toSql (site g), toSql (result g), toSql (annotation g)] | g <- games]
 
 createTableQuery = "CREATE TABLE games (\
                    \white VARCHAR(255) NOT NULL,\
@@ -27,3 +37,5 @@ createTableQuery = "CREATE TABLE games (\
                    \PRIMARY KEY (white, black));"
 
 deleteGamesQuery = "DELETE FROM games;"
+
+insertGameQuery = "INSERT INTO games VALUES (?, ?, ?, ?, ?, ?, ?);"

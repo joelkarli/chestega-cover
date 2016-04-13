10a07c9e424b66516a34b1271508f9fb37fa65c5
joelkarli <joel.karli91@gmail.com>
2016-04-13 23:07:29 +0200

    Generate cover with one game

--- src/Cover.hs
+++ src/Cover.hs
@@ -11,22 +11,16 @@ main = do
     args <- getArgs
     bytes <- B.readFile $ Prelude.head args
     conn <- connectSqlite3 $ Prelude.last args
-    Prelude.writeFile (Prelude.head args ++ ".pgn") "Test"
+    stmt <- prepare conn gameStartingWithQuery
+    executeMany stmt (prepCapsForQuery (capitals bytes))
+    rows <- fetchAllRows' stmt
+    Prelude.writeFile (Prelude.head args ++ ".pgn") (Prelude.concatMap (\g -> toPgn g) (rowsToGames rows))
     disconnect conn
 
-gameStr caps conn = Prelude.map (iogameToPgn) (capsToGames caps conn)
-            where iogameToPgn iog = do
-                            g <- iog
-                            return (toPgn g)
-
-capsToGames caps conn = Prelude.map (sqlGame conn) caps
-
-sqlGame conn c = do
-            stmt <- prepare conn gameStartingWithQuery
-            execute stmt [toSql (c : "%")]
-            rows <- fetchAllRows' stmt
-            return (convRow (Prelude.head rows))
+rowsToGames rows = Prelude.map convRow rows
         where convRow [sqlWhite, sqlBlack, sqlDate, sqlEvent, sqlSite, sqlResult, sqlRound, sqlAnnotation] = Game {white = fromSql sqlWhite, black = fromSql sqlBlack, date = fromSql sqlDate, event = fromSql sqlEvent, site = fromSql sqlSite, result = fromSql sqlResult, Game.round = fromSql sqlRound, annotation = fromSql sqlAnnotation}
+
+prepCapsForQuery caps = [[toSql (c : "%")] | c <- caps]
 
 gameStartingWithQuery = "SELECT white, black, date, event, site, result, round, annotation FROM games \
                         \WHERE white LIKE ? \

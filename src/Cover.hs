75e277ae5b10648b93593b32d2c52a02f3424727
joelkarli <joel.karli91@gmail.com>
2016-04-15 18:47:55 +0200

    Cover now outputs multiple games

--- src/Cover.hs
+++ src/Cover.hs
@@ -11,16 +11,15 @@ main = do
     args <- getArgs
     bytes <- B.readFile $ Prelude.head args
     conn <- connectSqlite3 $ Prelude.last args
-    stmt <- prepare conn gameStartingWithQuery
-    executeMany stmt (prepCapsForQuery (capitals bytes))
-    rows <- fetchAllRows' stmt
-    Prelude.writeFile (Prelude.head args ++ ".pgn") (Prelude.concatMap toPgn (rowsToGames rows))
+    games <- findGamesFromCaps (capitals bytes) conn []
+    Prelude.writeFile (Prelude.head args ++ ".pgn") (Prelude.concatMap toPgn games)
     disconnect conn
 
-rowsToGames = Prelude.map convRow
-        where convRow [sqlWhite, sqlBlack, sqlDate, sqlEvent, sqlSite, sqlResult, sqlRound, sqlAnnotation] = Game {white = fromSql sqlWhite, black = fromSql sqlBlack, date = fromSql sqlDate, event = fromSql sqlEvent, site = fromSql sqlSite, result = fromSql sqlResult, Game.round = fromSql sqlRound, annotation = fromSql sqlAnnotation}
-
-prepCapsForQuery caps = [[toSql (c : "%")] | c <- caps]
+findGamesFromCaps [] conn res = return $ Prelude.reverse res
+findGamesFromCaps (c : caps) conn res = do
+                                r <- quickQuery' conn gameStartingWithQuery [toSql (c : "%")]
+                                findGamesFromCaps caps conn ((convRow (Prelude.head r)) : res)
+                        where convRow [sqlWhite, sqlBlack, sqlDate, sqlEvent, sqlSite, sqlResult, sqlRound, sqlAnnotation] = Game {white = fromSql sqlWhite, black = fromSql sqlBlack, date = fromSql sqlDate, event = fromSql sqlEvent, site = fromSql sqlSite, result = fromSql sqlResult, Game.round = fromSql sqlRound, annotation = fromSql sqlAnnotation}
 
 gameStartingWithQuery = "SELECT white, black, date, event, site, result, round, annotation FROM games \
                         \WHERE white LIKE ? \

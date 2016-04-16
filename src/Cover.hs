b483e95908ce1a33471164b633d5d13faf775d56
joelkarli <joel.karli91@gmail.com>
2016-04-16 02:54:01 +0200

    Refactored findGamesFromCaps

--- src/Cover.hs
+++ src/Cover.hs
@@ -1,4 +1,5 @@
 import System.Environment
+import Data.Functor
 import Data.ByteString.Lazy as B
 import Database.HDBC
 import Database.HDBC.Sqlite3
@@ -17,8 +18,8 @@ main = do
 findGamesFromCaps [] conn res = return $ Prelude.reverse res
 findGamesFromCaps (c : caps) conn res = do
                                 r <- quickQuery' conn gameStartingWithQuery [toSql (c : "%")]
-                                findGamesFromCaps caps conn (convRow (Prelude.head r) : res)
-                        where convRow [sqlWhite, sqlBlack, sqlDate, sqlEvent, sqlSite, sqlResult, sqlRound, sqlAnnotation] = Game {white = fromSql sqlWhite, black = fromSql sqlBlack, date = fromSql sqlDate, event = fromSql sqlEvent, site = fromSql sqlSite, result = fromSql sqlResult, Game.round = fromSql sqlRound, annotation = fromSql sqlAnnotation}
+                                findGamesFromCaps caps conn (convRow (fromSql <$> (Prelude.head r)) : res)
+                        where convRow [qryWhite, qryBlack, qryDate, qryEvent, qrySite, qryResult, qryRound, qryAnnotation] = Game {white = qryWhite, black = qryBlack, date = qryDate, event = qryEvent, site = qrySite, result = qryResult, Game.round = qryRound, annotation = qryAnnotation}
 
 gameStartingWithQuery = "SELECT white, black, date, event, site, result, round, annotation FROM games \
                         \WHERE white LIKE ? \

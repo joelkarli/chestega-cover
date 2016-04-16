c12a6b1befc611e1afa8302001508dbb19d37a36
joelkarli <joel.karli91@gmail.com>
2016-04-16 20:04:34 +0200

    Add functions for uncovering

--- src/Encoding.hs
+++ src/Encoding.hs
@@ -1,7 +1,6 @@
 module Encoding
 ( capitals
-, charMapping
-, intMapping
+, uncapitals
 ) where
 
 import Data.List
@@ -10,11 +9,14 @@ import Data.Maybe (fromMaybe)
 import qualified Data.Map as M
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Word as W
-import Data.Bits ((.&.), shiftR)
+import Data.Bits ((.&.), (.|.), shiftR, shiftL)
 
 -- |Slices the ByteString, then maps each word to the corresponding Char using charMapping
 capitals :: B.ByteString -> String
 capitals bs = map (\w -> fromMaybe '0' (M.lookup w charMapping)) $ toInts $ slice bs
+
+uncapitals :: String -> B.ByteString
+uncapitals caps = B.pack (toWords (map (\c -> fromMaybe 16 (M.lookup c intMapping)) caps))
 
 -- |Slices a ByteString into pieces of 4 Bits
 slice :: B.ByteString -> [W.Word8]
@@ -33,6 +35,8 @@ firstBits w = (.&.) w 240
 
 -- |Returns the last 4 bits of w
 lastBits w = (.&.) w 15
+
+unify x y = (.|.) (shiftL x 4) y
 
 -- |Maps the integers from 0 to 15 to chars A to P
 charMapping :: M.Map Int Char
--- src/Uncover.hs
+++ src/Uncover.hs
@@ -1,7 +1,10 @@
 import System.Environment
+import Data.ByteString.Lazy as B
+import PGN
+import Encoding
 
 main :: IO()
 main = do
     args <- getArgs
-    f <- readFile $ head args
-    putStrLn "Test"
+    f <- Prelude.readFile $ Prelude.head args
+    B.writeFile (Prelude.last args) (uncapitals (pgnToWhiteCaps (Prelude.head args) f))

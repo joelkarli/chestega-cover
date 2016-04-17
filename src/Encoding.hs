17fb7d7fd2eb020c7511210e2fdf04e440c74d24
joelkarli <joel.karli91@gmail.com>
2016-04-17 16:20:05 +0200

    Unslice now works properly

--- src/Encoding.hs
+++ src/Encoding.hs
@@ -15,21 +15,18 @@ import Data.Bits ((.&.), (.|.), shiftR, shiftL)
 capitals :: B.ByteString -> String
 capitals bs = map (\w -> fromMaybe '0' (M.lookup w charMapping)) $ toInts $ slice bs
 
+-- |Basically reverses capitals to a ByteString by mapping each Char to the corresponding word and then unslicing
 uncapitals :: String -> B.ByteString
-uncapitals caps = unslice (toWords (map (\c -> fromMaybe 16 (M.lookup c intMapping)) caps))
+uncapitals caps = B.pack (unslice (toWords (map (\c -> fromMaybe 16 (M.lookup c intMapping)) caps)))
 
 -- |Slices a ByteString into pieces of 4 Bits
 slice :: B.ByteString -> [W.Word8]
 slice bs = concatMap (\w -> [shiftR (firstBits w) 4, lastBits w]) $ B.unpack bs
 
-unslice :: [W.Word8] -> B.ByteString
-unslice ws = B.pack (zipWith (unify) (everySnd ws) (everySnd' ws))
-
-everySnd [] = []
-everySnd (x:xs) = x : (everySnd (tail xs)) 
-
-everySnd' [] = []
-everySnd' (x:xs) = (head xs) : (everySnd (tail xs))
+-- |Reverses the slice function. Unifies all the 4 Bit Words to 8 Bit Words
+unslice :: [W.Word8] -> [W.Word8]
+unslice [] = []
+unslice (w:ws) = (unify w (head ws)) : (unslice (tail ws))
 
 -- |Converts a list of Word8 to a list of Int
 toInts :: [W.Word8] -> [Int]
@@ -45,6 +42,7 @@ firstBits w = (.&.) w 240
 -- |Returns the last 4 bits of w
 lastBits w = (.&.) w 15
 
+-- |Unifies two 4 Bit Words to an 8 Bit Word
 unify x y = (.|.) (shiftL x 4) y
 
 -- |Maps the integers from 0 to 15 to chars A to P

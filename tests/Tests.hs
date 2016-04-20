fda112478d04685e2e77f7d26f9393033ed09551
joelkarli <joel.karli91@gmail.com>
2016-04-20 21:10:33 +0200

    Work on test suite

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -12,5 +12,13 @@ prop_noLast4Bits =
     forAll wordGen
     (\w -> lastBits (firstBits w) == 0)
 
+prop_noFirst4Bits :: Property
+prop_noFirst4Bits =
+    forAll wordGen
+    (\w -> firstBits (lastBits w) == 0)
+
+properties :: [Property]
+properties = [prop_noLast4Bits, prop_noFirst4Bits]
+
 main :: IO ()
-main = quickCheck prop_noLast4Bits
+main = mapM_ quickCheck properties
